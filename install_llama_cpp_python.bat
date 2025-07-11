e0d032839f58f807bb80e66592fd59b4fc44d5b2
okazaki10 <rakaabadisusilo@gmail.com>
2025-07-11 16:53:04 +0700

    add llama cpp python

--- install_llama_cpp_python.bat
+++ install_llama_cpp_python.bat
@@ -2,6 +2,6 @@
 SET DIR=%~dp0%
 %DIR%\installer_files\env\python -m pip install ninja && %DIR%\installer_files\env\python -m pip install py-cpuinfo==9.0.0
 %DIR%\installer_files\env\python -m pip install torch==2.7.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
-@REM set CMAKE_ARGS="-DGGML_CUDA=on" && %DIR%\installer_files\env\python -m pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
+set CMAKE_ARGS="-DGGML_CUDA=on" && %DIR%\installer_files\env\python -m pip install llama-cpp-python --upgrade --force-reinstall --no-cache-dir
 echo done installing pytorch and llama cpp python
 %DIR%\install_depedency.bat
