6f1a96eb9623931ceeda262f6af1e9721757774e
okazaki10 <rakaabadisusilo@gmail.com>
2025-07-13 23:18:22 +0700

    fix fairseq

--- install_fairseq.bat
+++ install_fairseq.bat
@@ -2,6 +2,6 @@
 SET DIR=%~dp0%
 call refreshenv
 echo %CUDA_PATH%
-%DIR%\installer_files\env\python -m pip install --no-cache-dir --no-build-isolation git+https://github.com/okazaki10/fairseq.git@main
-echo done installing depedency, if you encountered error, open install_depedency.bat again
+%DIR%\installer_files\env\python -m pip install https://github.com/okazaki10/fairseq/releases/download/v.1.0-python-3.11-pytorch-2.7.0/fairseq-0.12.2-cp311-cp311-win_amd64.whl
+echo done installing depedency, if you encountered error, open install_fairseq.bat again
 %DIR%\add_models.bat
