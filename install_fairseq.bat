c342aaec7c5154ca6f692bef00196689c10790c9
okazaki10 <rakaabadisusilo@gmail.com>
2025-07-13 21:42:07 +0700

    fix fairseq

--- install_fairseq.bat
+++ install_fairseq.bat
@@ -1,6 +1,7 @@
 
 SET DIR=%~dp0%
 call refreshenv
-%DIR%\installer_files\env\python -m pip install git+https://github.com/okazaki10/fairseq.git@main
+echo %CUDA_PATH%
+%DIR%\installer_files\env\python -m pip install --no-build-isolation git+https://github.com/okazaki10/fairseq.git@main
 echo done installing depedency, if you encountered error, open install_depedency.bat again
 %DIR%\add_models.bat
