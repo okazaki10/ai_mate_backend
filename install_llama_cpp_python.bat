73470d1f0134da2444b47209719c1d8bb56d4e9d
okazaki10 <rakaabadisusilo@gmail.com>
2025-07-20 21:57:57 +0700

    revert test

--- install_llama_cpp_python.bat
+++ install_llama_cpp_python.bat
@@ -2,7 +2,7 @@
 SET DIR=%~dp0%
 %DIR%\installer_files\env\python -m pip install ninja && %DIR%\installer_files\env\python -m pip install py-cpuinfo==9.0.0
 %DIR%\installer_files\env\python -m pip install torch==2.7.1 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
-@REM set CMAKE_ARGS="-DGGML_CUDA=on" && %DIR%\installer_files\env\python -m pip install llama-cpp-python==0.3.9 --upgrade --force-reinstall --no-cache-dir
-%DIR%\installer_files\env\python -m pip install https://github.com/okazaki10/llama-cpp-python/releases/download/0.3.9/llama_cpp_python-0.3.9-cp311-cp311-win_amd64.whl
+set CMAKE_ARGS="-DGGML_CUDA=on" && %DIR%\installer_files\env\python -m pip install llama-cpp-python==0.3.9 --upgrade --force-reinstall --no-cache-dir
+@REM %DIR%\installer_files\env\python -m pip install https://github.com/okazaki10/llama-cpp-python/releases/download/0.3.9/llama_cpp_python-0.3.9-cp311-cp311-win_amd64.whl
 echo done installing pytorch and llama cpp python, if you encountered error, open install_llama_cpp_python.bat again
 %DIR%\install_depedency.bat
